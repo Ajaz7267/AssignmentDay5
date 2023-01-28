0671308774026f42384e8c848ddc47e55f0c3fab
Ansari Azaz Ahmad <121128619+Ajaz7267@users.noreply.github.com>
2023-01-28 14:36:34 +0530

    HARMONIC POWEROF2 QUOTIENT

---
+++ HarmonicNumber.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+import java.util.Scanner;
+public class HarmonicNumber {
+
+
+
+
+        static double harmonic(int N)
+        {
+
+            float value = 1;
+
+
+            for (int i = 2; i <= N; i++) {
+                value += (float)1 / i;
+            }
+
+            return value;
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter value of N");
+            int N = sc.nextInt();
+            if(N==0) {
+                System.out.println("enter a ositive integer value");
+            }
+
+            System.out.println(harmonic(N));
+
+
+
+        }
+    }
---
+++ PowerOf2.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+import java.util.Scanner;
+import java.lang.Math;
+public class PowerOf2 {
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter value of N");
+
+
+            int n= sc.nextInt();
+            for(int i =1; i<=n;i++){
+                System.out.println("table of power of 2 is " + (int)Math.pow(2, i));
+
+
+            }
+
+        }
+
+
+    }
+
